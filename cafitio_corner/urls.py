e67d69ef3a56ea3000a8cd595fdfb7fb4d9d542b
Alex Terinte <terintealexandrin18@gmail.com>
2024-07-15 12:29:39 +0000

    Created the 404 template

--- cafitio_corner/urls.py
+++ cafitio_corner/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from .views import handler404
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -33,3 +34,5 @@ urlpatterns = [
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'cafitio_corner.views.handler404'
