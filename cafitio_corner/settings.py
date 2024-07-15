2c8d326a43f6c2a3398faa90bc848ff7084a204a
Alex Terinte <terintealexandrin18@gmail.com>
2024-07-15 21:10:38 +0000

    Removed secret key and set debug

--- cafitio_corner/settings.py
+++ cafitio_corner/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)6^k__haqj$q4q!-8g1vhfn46ajrklh+ccftu*@!&wy5xh(-+#'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['cafitio-corner.herokuapp.com', 'localhost', '8000-terintealex-cafitiocorn-bxorgs2uthj.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['cafitio-corner-5f01d0c35567.herokuapp.com', 'localhost', '8000-terintealex-cafitiocorn-bxorgs2uthj.ws.codeinstitute-ide.net']
 
 
 # Application definition
