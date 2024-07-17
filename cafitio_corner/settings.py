87b97e084ea22b065bc3fd521880b40f7a577d22
Alex Terinte <terintealexandrin18@gmail.com>
2024-07-17 20:16:48 +0000

    Changing the debugs to development

--- cafitio_corner/settings.py
+++ cafitio_corner/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['cafitio-corner-5f01d0c35567.herokuapp.com', 'localhost', '8000-terintealex-cafitiocorn-bxorgs2uthj.ws.codeinstitute-ide.net']
 
