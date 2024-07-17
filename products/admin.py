8a6c559453c451b371e57191c9be020e45d082a6
Alex Terinte <terintealexandrin18@gmail.com>
2024-07-17 19:28:37 +0000

    Added summernote urls to the maine urls file

--- products/admin.py
+++ products/admin.py
@@ -2,11 +2,8 @@ from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from .models import Product, Category, Review
 
-# Register your models here.
-
-
 class ProductAdmin(SummernoteModelAdmin):
-    summernote_fields = ('description',) 
+    summernote_fields = ('description',)
 
     list_display = (
         'sku',
@@ -18,7 +15,6 @@ class ProductAdmin(SummernoteModelAdmin):
     )
     
     ordering = ('sku',)
-
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('friendly_name', 'name', 'parent')
@@ -34,12 +30,10 @@ class CategoryAdmin(admin.ModelAdmin):
     parent.admin_order_field = 'parent__friendly_name'
     parent.short_description = 'Parent Category'
 
-
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ('product', 'user', 'rating', 'comment', 'created_at')
     list_filter = ('rating', 'created_at')
     search_fields = ('product__name', 'user__username', 'comment')
-
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
