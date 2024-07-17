799822f8a98209574933493bb44264970647ab22
Alex Terinte <terintealexandrin18@gmail.com>
2024-07-17 12:24:13 +0000

    Changed the default emails in settings

--- cafitio_corner/settings.py
+++ cafitio_corner/settings.py
@@ -236,7 +236,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'hookahculture@example.com'
+    DEFAULT_FROM_EMAIL = 'cafitiocorner@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
