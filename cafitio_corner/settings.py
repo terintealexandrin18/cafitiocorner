69bddd3dc14aa7ff0fd8d2347ed5e1a8910ed6ba
Alex Terinte <terintealexandrin18@gmail.com>
2024-07-21 13:39:10 +0000

    Set the debug to false for deployment

--- cafitio_corner/settings.py
+++ cafitio_corner/settings.py
@@ -29,8 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-#DEBUG = 'DEVELOPMENT' in os.environ
+#DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     'cafitio-corner-5f01d0c35567.herokuapp.com', 'localhost',
