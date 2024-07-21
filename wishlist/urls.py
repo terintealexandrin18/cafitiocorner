28ffe0fd879e6e0d79682ddfaca36d813e0d69ac
Alex Terinte <terintealexandrin18@gmail.com>
2024-07-21 09:44:34 +0000

    Remove the duplicate wishlist urls

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.wishlist, name='wishlist'),
-    path('wishlist/', views.wishlist, name='wishlist'),
     path(
         'toggle_wishlist/<int:product_id>/',
         views.toggle_wishlist,
